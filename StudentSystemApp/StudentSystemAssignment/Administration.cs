191ad98bd99ae62cab7c1e5e02006053a497160a
Daniil <66083382+daniil2231@users.noreply.github.com>
2023-01-10 14:23:23 +0100

    Student system app
    A C# windows forms app where you can give credits to students, advance them in their school years, promote teachers to higher positions, etc.

---
+++ StudentSystemApp/StudentSystemAssignment/Administration.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSystemAssignment
+{
+    public class Administration
+    {
+        private List<Person> persons;
+
+        public Administration()
+        {
+            persons = new List<Person>();
+        }
+
+        public void AddPerson(Person p)
+        {
+            persons.Add(p);
+        }
+
+        public List<Person> CheckForPartialName(string partialName)
+        {
+            List<Person> persons = new List<Person>();
+
+            foreach (Person person in GetPersons())
+            {
+                if (person.Name.Contains(partialName))
+                {
+                    persons.Add(person);
+                }
+            }
+            return persons;
+        }
+
+        public Person GetPersonByPCN(int pcn)
+        {
+            foreach (Person person in GetPersons())
+            {
+                if (person.Pcn == pcn)
+                {
+                    return person;
+                }
+            }
+
+            return null;
+        }
+
+        public List<Person> GetPersons()
+        {
+            return persons;
+        }
+    }
+}
---
+++ StudentSystemApp/StudentSystemAssignment/JobPosition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSystemAssignment
+{
+    public enum JobPosition
+    {
+        JUNIOR_TEACHER,
+        TEACHER1,
+        TEACHER2,
+        TEACHER3,
+        COORDINATOR,
+        DIRECTOR
+    }
+}
